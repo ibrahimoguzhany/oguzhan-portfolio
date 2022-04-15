61863525bee4cb6e8b93d808845748ed46b8eb2e
ibrahimoguzhany <ibrahimoguzhany@gmail.com>
2022-04-15 21:00:02 +0300

    apolloServer refactor

--- server/database/models/portfolio.js
+++ server/database/models/portfolio.js
@@ -12,6 +12,7 @@ const portfolioSchema = new Schema({
     startDate: { type: Date, required: true },
     endDate: Date,
     createdAt: { type: Date, default: Date.now }
-})
+});
+
 
 module.exports = mongoose.model('Portfolio', portfolioSchema);
