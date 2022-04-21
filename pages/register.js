6d61b1a2da36158e22867ff7d79a9532d2772519
ibrahimoguzhany <ibrahimoguzhany@gmail.com>
2022-04-21 16:01:14 +0300

    RegisterForm and register page infrastructure

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,24 @@
+import RegisterForm from '@/components/forms/RegisterForm';
 
 
 const Register = () => {
-    return <h1>register</h1>
-}
-export default Register
+
+    const register = (registerData) => {
+        alert(JSON.stringify(registerData));
+    };
+
+    return (
+        <>
+            <div className="bwm-form mt-5">
+                <div className="row">
+                    <div className="col-md-5 mx-auto">
+                        <h1 className="page-title">Register</h1>
+                        <RegisterForm onSubmit={register} />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Register;
